0f028b692bd6e16bf920e47c9032bf91d1e37f8d
Gizem Ocak <Gizem@Gizems-MacBook-Air.local>
2019-03-28 10:15:55 -0400

    Added genres to the Film Details

--- src/components/FilmDetails.jsx
+++ src/components/FilmDetails.jsx
@@ -10,16 +10,26 @@ const FilmDetails = inject("filmStore")(
         console.log("selected film", toJS(this.props.filmStore.selectedFilm));
       }
 
+      getGenres = genreIds => {
+        if (!genreIds) return null;
+
+        return genreIds.map(genreId => {
+          const genre = genres.find(g => g.id === genreId);
+          return !genre ? null : (
+            <div key={genreId}>
+              <ul>{genre.name}</ul>
+            </div>
+          );
+        });
+      };
+
       render() {
-        console.log(
-          "this.props",
-          this.props.match.params.id.replace(/_/g, " ")
-        );
         return (
           <div>
-            <h1>Film Detail Page</h1>
+            <h1>Film Details</h1>
             <p>{this.props.filmStore.selectedFilm.original_title}</p>
             <p>{this.props.filmStore.selectedFilm.overview}</p>
+            {this.getGenres(this.props.filmStore.selectedFilm.genre_ids)}
           </div>
         );
       }
